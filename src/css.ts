cbd681060f9e495235655df52347b9068fbead9f
Marijn Haverbeke <marijn@haverbeke.nl>
2021-08-11 08:32:28 +0200

    Follow LezerLanguage -> LRLanguage rename

--- src/css.ts
+++ src/css.ts
@@ -1,5 +1,5 @@
 import {parser} from "@lezer/css"
-import {LezerLanguage, continuedIndent, indentNodeProp, foldNodeProp, foldInside, LanguageSupport} from "@codemirror/language"
+import {LRLanguage, continuedIndent, indentNodeProp, foldNodeProp, foldInside, LanguageSupport} from "@codemirror/language"
 import {styleTags, tags as t} from "@codemirror/highlight"
 import {Extension} from "@codemirror/state"
 import {completeCSS} from "./complete"
@@ -7,7 +7,7 @@ import {completeCSS} from "./complete"
 /// A language provider based on the [Lezer CSS
 /// parser](https://github.com/lezer-parser/css), extended with
 /// highlighting and indentation information.
-export const cssLanguage = LezerLanguage.define({
+export const cssLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
