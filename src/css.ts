acf7e98b6e8d33414d8050886868493d20c55475
Marijn Haverbeke <marijn@haverbeke.nl>
2022-03-24 10:02:25 +0100

    Move highlighting information into @lezer/css

--- src/css.ts
+++ src/css.ts
@@ -1,6 +1,5 @@
 import {parser} from "@lezer/css"
 import {LRLanguage, continuedIndent, indentNodeProp, foldNodeProp, foldInside, LanguageSupport} from "@codemirror/language"
-import {styleTags, tags as t} from "@codemirror/highlight"
 import {Extension} from "@codemirror/state"
 import {cssCompletionSource} from "./complete"
 export {cssCompletionSource} from "./complete"
@@ -16,42 +15,6 @@ export const cssLanguage = LRLanguage.define({
       }),
       foldNodeProp.add({
         Block: foldInside
-      }),
-      styleTags({
-        "import charset namespace keyframes": t.definitionKeyword,
-        "media supports": t.controlKeyword,
-        "from to selector": t.keyword,
-        NamespaceName: t.namespace,
-        KeyframeName: t.labelName,
-        TagName: t.tagName,
-        ClassName: t.className,
-        PseudoClassName: t.constant(t.className),
-        IdName: t.labelName,
-        "FeatureName PropertyName": t.propertyName,
-        AttributeName: t.attributeName,
-        NumberLiteral: t.number,
-        KeywordQuery: t.keyword,
-        UnaryQueryOp: t.operatorKeyword,
-        "CallTag ValueName": t.atom,
-        VariableName: t.variableName,
-        Callee: t.operatorKeyword,
-        Unit: t.unit,
-        "UniversalSelector NestingSelector": t.definitionOperator,
-        AtKeyword: t.keyword,
-        MatchOp: t.compareOperator,
-        "ChildOp SiblingOp, LogicOp": t.logicOperator,
-        BinOp: t.arithmeticOperator,
-        Important: t.modifier,
-        Comment: t.blockComment,
-        ParenthesizedContent: t.special(t.name),
-        ColorLiteral: t.color,
-        StringLiteral: t.string,
-        ":": t.punctuation,
-        "PseudoOp #": t.derefOperator,
-        "; ,": t.separator,
-        "( )": t.paren,
-        "[ ]": t.squareBracket,
-        "{ }": t.brace
       })
     ]
   }),
