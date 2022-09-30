6779bfa2a153ccb38e4465d21a0629fec4d7451b
Michał Niedziółka <niedziolek@hotmail.com>
2022-09-30 14:26:20 +0200

    Missing pseudo-classes

--- src/complete.ts
+++ src/complete.ts
@@ -14,14 +14,17 @@ function properties() {
 }
 
 const pseudoClasses = [
-  "active", "after", "before", "checked", "default",
-  "disabled", "empty", "enabled", "first-child", "first-letter",
-  "first-line", "first-of-type", "focus", "hover", "in-range",
-  "indeterminate", "invalid", "lang", "last-child", "last-of-type",
-  "link", "not", "nth-child", "nth-last-child", "nth-last-of-type",
-  "nth-of-type", "only-of-type", "only-child", "optional", "out-of-range",
-  "placeholder", "read-only", "read-write", "required", "root",
-  "selection", "target", "valid", "visited"
+  "active", "after", "any-link", "autofill", "before", "checked",
+  "default", "defined", "disabled", "empty", "enabled", "first",
+  "first-child", "first-letter", "first-line", "first-of-type",
+  "focus", "focus-visible", "focus-within", "fullscreen", "has",
+  "host", "host-context", "hover", "in-range", "indeterminate",
+  "invalid", "is", "lang", "last-child", "last-of-type", "left",
+  "link", "modal", "not", "nth-child", "nth-last-child",
+  "nth-last-of-type", "nth-of-type", "only-child", "only-of-type",
+  "optional", "out-of-range", "placeholder", "placeholder-shown",
+  "read-only", "read-write", "required", "right", "root", "scope",
+  "selection", "target", "valid", "visited", "where"
 ].map(name => ({type: "class", label: name}))
 
 const values = [
